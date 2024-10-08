cf4f9427617899e178368d9a0dcb56b98887fc12
Shivansh-Rastogi094 <rshivansh094@gmail.com>
2024-10-08 15:21:41 +0530

    2

--- Day4/script.js
+++ Day4/script.js
@@ -1,11 +1,24 @@
 
-let y=18 // by changing the scope of the variable
-function display()
-{   
-    let a =15;
-    console.log(a);
-    let j=14
-    console.log(j);
-}
-
-display()
+// let y=18 // by changing the scope of the variable
+// function display()
+// {   
+//     let a =15;
+//     console.log(a);
+//     let j=14
+//     console.log(j);
+// }
+function validate()
+ {
+    // alert("Validated");
+    let User=document.getElementById('Un').value
+    let Pass=document.getElementById('pass').value
+    let p1 =document.getElementById('result');
+    if(User=="admin" && Pass=="admin")
+    {
+        p1.innerText = "Successfully Validated";
+    }
+    else{
+        p1.innerText = "Not Validated";
+    }
+    
+ }
